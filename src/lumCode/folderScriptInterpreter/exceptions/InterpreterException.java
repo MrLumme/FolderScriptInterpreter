1a1c08e2e4f5192252979dd30c9baa9d8e3cd11f
Lumme Thomsen <mclumme@gmail.com>
2020-06-30 23:34:09 +0200

    Added base exception

---
+++ src/lumCode/folderScriptInterpreter/exceptions/InterpreterException.java
@@ -0,0 +1,13 @@
+package lumCode.folderScriptInterpreter.exceptions;
+
+public class InterpreterException extends Exception {
+	private static final long serialVersionUID = 4169904615141147003L;
+
+	public InterpreterException() {
+	}
+
+	public InterpreterException(String message) {
+		super(message);
+	}
+
+}
