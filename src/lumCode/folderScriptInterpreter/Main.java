d494369b651bbe8757340e9badd8100a71c16ec7
Lumme Thomsen <mclumme@gmail.com>
2020-06-30 23:34:30 +0200

    - Updated interpreter

--- src/lumCode/folderScriptInterpreter/Main.java
+++ src/lumCode/folderScriptInterpreter/Main.java
@@ -1,9 +1,44 @@
 package lumCode.folderScriptInterpreter;
 
+import java.io.File;
+import java.util.ArrayList;
+
+import lumCode.folderScriptInterpreter.variables.DoubleVariable;
+import lumCode.folderScriptInterpreter.variables.FileVariable;
+import lumCode.folderScriptInterpreter.variables.FolderVariable;
+import lumCode.folderScriptInterpreter.variables.IntVariable;
+import lumCode.folderScriptInterpreter.variables.StringVariable;
+import lumCode.folderScriptInterpreter.variables.Variable;
+
 public class Main {
+	public ArrayList<Variable> vars = new ArrayList<>();
+	public boolean overwrite = false;
+
+	public static String script = "";
+	public static Variable[] a;
 
 	public static void main(String[] args) {
-		// Interpret
+		script = args[0];
+		a = new Variable[args.length - 1];
+		for (int i = 1; i < args.length; i++) {
+			a[i - 1] = makeVariable(args[i]);
+		}
+	}
+
+	private static Variable makeVariable(String var) {
+		if (var.matches("^[0-9]{1,}$")) {
+			return new IntVariable(Integer.parseInt(var));
+		} else if (var.matches("^[0-9.]{1,}$")) {
+			return new DoubleVariable(Double.parseDouble(var));
+		} else if (var.matches("^[A-Z]{1}:(\\\\|\\/)")) {
+			if (var.contains(".")) {
+				return new FileVariable(new File(var));
+			} else {
+				return new FolderVariable(new File(var));
+			}
+		} else {
+			return new StringVariable(var);
+		}
 	}
 
 }
