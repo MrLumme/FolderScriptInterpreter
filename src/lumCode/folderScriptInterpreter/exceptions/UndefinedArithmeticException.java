2e94fcf8f70b4342a14e2722a35401ff258da61a
Lumme Thomsen <mclumme@gmail.com>
2020-07-02 22:51:01 +0200

    - Added exceptions and made some type exception super class structure

---
+++ src/lumCode/folderScriptInterpreter/exceptions/UndefinedArithmeticException.java
@@ -0,0 +1,10 @@
+package lumCode.folderScriptInterpreter.exceptions;
+
+public class UndefinedArithmeticException extends InterpreterException {
+	private static final long serialVersionUID = -675794863639635267L;
+
+	public UndefinedArithmeticException(String message) {
+		super(message);
+	}
+
+}
---
+++ src/lumCode/folderScriptInterpreter/exceptions/UnsupportedTypeException.java
@@ -0,0 +1,11 @@
+package lumCode.folderScriptInterpreter.exceptions;
+
+public class UnsupportedTypeException extends InterpreterException {
+	private static final long serialVersionUID = -8540818204257617736L;
+
+	public UnsupportedTypeException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
