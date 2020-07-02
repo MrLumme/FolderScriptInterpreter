de0f6b50f999291cd6090b8727101fe044eb53ca
Lumme Thomsen <mclumme@gmail.com>
2020-07-02 22:51:53 +0200

    - Removed negate from ArithmeticType - Added toChar and fromChar method to ArithmeticType

--- src/lumCode/folderScriptInterpreter/handlers/ArithmeticType.java
+++ src/lumCode/folderScriptInterpreter/handlers/ArithmeticType.java
@@ -1,5 +1,39 @@
 package lumCode.folderScriptInterpreter.handlers;
 
+import lumCode.folderScriptInterpreter.exceptions.UnsupportedArithmeticTypeException;
+
 public enum ArithmeticType {
-	ADDITION, SUBTRACTION, MULTIPLICATION, DIVISION, MODULO, NEGATE
+	ADDITION, SUBTRACTION, MULTIPLICATION, DIVISION, MODULO;
+
+	public static ArithmeticType fromChar(char c) throws UnsupportedArithmeticTypeException {
+		switch (c) {
+		case '+':
+			return ADDITION;
+		case '/':
+			return DIVISION;
+		case '%':
+			return MODULO;
+		case '*':
+			return MULTIPLICATION;
+		case '-':
+			return SUBTRACTION;
+		}
+		throw new UnsupportedArithmeticTypeException("The operator '" + c + "' is not supported.");
+	}
+
+	public static char toChar(ArithmeticType c) throws UnsupportedArithmeticTypeException {
+		switch (c) {
+		case ADDITION:
+			return '+';
+		case DIVISION:
+			return '/';
+		case MODULO:
+			return '%';
+		case MULTIPLICATION:
+			return '*';
+		case SUBTRACTION:
+			return '-';
+		}
+		throw new UnsupportedArithmeticTypeException("The operator '" + c.toString() + "' is not supported.");
+	}
 }
