fc62668199d4f70b6c1b3612929aa08eacf88cad
MrLumme <mclumme@gmail.com>
2020-07-20 12:50:41 +0200

    - Updated IteratorHandler

--- src/lumCode/folderScriptInterpreter/handlers/IteratorHandler.java
+++ src/lumCode/folderScriptInterpreter/handlers/IteratorHandler.java
@@ -5,6 +5,7 @@ import java.io.File;
 import lumCode.folderScriptInterpreter.Main;
 import lumCode.folderScriptInterpreter.exceptions.InfiniteLoopException;
 import lumCode.folderScriptInterpreter.exceptions.IteratorTypeException;
+import lumCode.folderScriptInterpreter.variables.FileVariable;
 import lumCode.folderScriptInterpreter.variables.FolderVariable;
 import lumCode.folderScriptInterpreter.variables.IntVariable;
 import lumCode.folderScriptInterpreter.variables.StringVariable;
@@ -21,31 +22,34 @@ public class IteratorHandler {
 				throw new InfiniteLoopException((IntVariable) rule);
 			}
 
-			IntVariable val = new IntVariable(0);
-			Main.i[number] = val;
-
-			while (val.getVar() < till) {
+			Main.i[number] = new IntVariable(0);
+			while (((IntVariable) Main.i[number]).getVar() < till) {
 
 				// iterate
 
-				val.setVar(val.getVar() + 1);
+				((IntVariable) Main.i[number]).setVar(((IntVariable) Main.i[number]).getVar() + 1);
 			}
 		} else if (rule instanceof FolderVariable) {
 			File[] list = ((FolderVariable) rule).getVar().listFiles();
+			Main.i[number] = new FileVariable(null);
 			for (File f : list) {
-				// Add file val
+				((FileVariable) Main.i[number]).setVar(f);
 
 				// iterate
+
 			}
 		} else if (rule instanceof StringVariable) {
-			String key = ((StringVariable) rule).getVar();
-			int point = 0;
-			char val = key.charAt(point);
+			char[] seq = ((StringVariable) rule).getVar().toCharArray();
+			Main.i[number] = new StringVariable("");
+			for (char c : seq) {
+				((StringVariable) Main.i[number]).setVar("" + c);
 
-			// iterate
+				// iterate
 
+			}
 		} else {
 			throw new IteratorTypeException(rule);
 		}
+		Main.i[number] = null;
 	}
 }
