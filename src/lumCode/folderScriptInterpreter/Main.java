3dad33ce56f1d8ea2752d2bb90fbcee83cbd3cfb
MrLumme <mclumme@gmail.com>
2020-07-22 16:07:08 +0200

    - Updated Main

--- src/lumCode/folderScriptInterpreter/Main.java
+++ src/lumCode/folderScriptInterpreter/Main.java
@@ -2,15 +2,20 @@ package lumCode.folderScriptInterpreter;
 
 import java.util.ArrayList;
 
+import lumCode.folderScriptInterpreter.handlers.Node;
+import lumCode.folderScriptInterpreter.handlers.arithmetic.ArithmeticType;
+import lumCode.folderScriptInterpreter.handlers.command.CommandType;
+import lumCode.folderScriptInterpreter.handlers.logic.LogicType;
 import lumCode.folderScriptInterpreter.variables.Variable;
 
 public class Main {
 	public static boolean overwrite = false;
+	public static boolean caseSensitive = false;
 	public static String script = "";
 	public static Variable[] a;
 	public static Variable[] i;
-
-	public static ArrayList<Variable> vars = new ArrayList<>();
+	public static final ArrayList<Variable> vars = new ArrayList<>();
+	public static final ArrayList<Node> nodes = new ArrayList<>();
 
 	public static void main(String[] args) {
 		script = args[0];
@@ -19,12 +24,30 @@ public class Main {
 			a[i - 1] = Variable.fromString(args[i]);
 		}
 
-		String[] list = breakDownScript();
+		breakDownScript();
 
 	}
 
-	private static String[] breakDownScript() {
-		return null;
+	private static void breakDownScript() {
+		char[] c = script.toCharArray();
+
+		for (int i = 0; i < c.length; i++) {
+			if (CommandType.valid(c[i])) {
+
+			} else if (ArithmeticType.valid(c[i])) {
+
+			} else if (LogicType.valid(c[i])) {
+
+			} else if (c[i] == 'i') {
+
+			} else if (c[i] == 'h') {
+
+			} else if (c[i] == '#') {
+
+			} else if (c[i] == 'x') {
+
+			}
+		}
 	}
 
 	public static Variable lookUpVariable(String name) {
