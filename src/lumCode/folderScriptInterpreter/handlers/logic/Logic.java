4cd4b8ba63a1a53537e8386cc61a46b51b7429de
MrLumme <mclumme@gmail.com>
2020-07-23 16:22:07 +0200

    - Renamed LogicHandler - Added LogicNode

--- src/lumCode/folderScriptInterpreter/handlers/logic/Logic.java
+++ src/lumCode/folderScriptInterpreter/handlers/logic/Logic.java
@@ -1,7 +1,7 @@
 package lumCode.folderScriptInterpreter.handlers.logic;
 
+import lumCode.folderScriptInterpreter.exceptions.InterpreterException;
 import lumCode.folderScriptInterpreter.exceptions.UndefinedLogicException;
-import lumCode.folderScriptInterpreter.exceptions.UnsupportedLogicTypeException;
 import lumCode.folderScriptInterpreter.variables.FileVariable;
 import lumCode.folderScriptInterpreter.variables.FolderVariable;
 import lumCode.folderScriptInterpreter.variables.IntVariable;
@@ -9,32 +9,51 @@ import lumCode.folderScriptInterpreter.variables.StringVariable;
 import lumCode.folderScriptInterpreter.variables.Variable;
 import lumCode.folderScriptInterpreter.variables.VariableType;
 
-public class LogicHandler {
+public class Logic implements LogicNode {
+	private final Variable left, right;
+	private final LogicType type;
+	private boolean result;
 
-	public static boolean Interpret(Variable left, LogicType operator, Variable right)
-			throws UndefinedLogicException, UnsupportedLogicTypeException {
+	public Logic(Variable left, LogicType type, Variable right) {
+		this.left = left;
+		this.right = right;
+		this.type = type;
+	}
+
+	@Override
+	public void action() throws InterpreterException {
 		if (left.type == VariableType.INT) {
 			if (right.type == VariableType.INT) {
-				return NumberLogicHandler.Interpret(((IntVariable) left).getVar(), operator,
+				result = NumberLogicHandler.Interpret(((IntVariable) left).getVar(), type,
 						((IntVariable) right).getVar());
 			}
 		} else if (left.type == VariableType.STRING) {
 			if (right.type == VariableType.STRING) {
-				return StringLogicHandler.Interpret(((StringVariable) left).getVar(), operator,
+				result = StringLogicHandler.Interpret(((StringVariable) left).getVar(), type,
 						((StringVariable) right).getVar());
 			}
 		} else if (left.type == VariableType.FILE) {
 			if (right.type == VariableType.FILE) {
-				return FileLogicHandler.Interpret(((FileVariable) left).getVar(), operator,
+				result = FileLogicHandler.Interpret(((FileVariable) left).getVar(), type,
 						((FileVariable) right).getVar());
 			}
 		} else if (left.type == VariableType.FOLDER) {
 			if (right.type == VariableType.FOLDER) {
-				return FolderLogicHandler.Interpret(((FolderVariable) left).getVar(), operator,
+				result = FolderLogicHandler.Interpret(((FolderVariable) left).getVar(), type,
 						((FolderVariable) right).getVar());
 			}
 		}
-		throw new UndefinedLogicException(left.toString(), operator, right.toString());
+		throw new UndefinedLogicException(left.toString(), type, right.toString());
 	}
 
+	@Override
+	public void explain() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean result() {
+		return result;
+	}
 }
---
+++ src/lumCode/folderScriptInterpreter/handlers/logic/LogicNode.java
@@ -0,0 +1,8 @@
+package lumCode.folderScriptInterpreter.handlers.logic;
+
+import lumCode.folderScriptInterpreter.handlers.Node;
+
+public interface LogicNode extends Node {
+
+	public boolean result();
+}
