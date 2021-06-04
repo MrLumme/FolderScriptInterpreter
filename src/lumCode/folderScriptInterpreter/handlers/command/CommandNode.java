59753ad69ae86f822af1d92fc4615636349dba8c
MrLumme <mclumme@gmail.com>
2021-06-04 15:51:15 +0200

    - Removed nodes

--- src/lumCode/folderScriptInterpreter/handlers/command/CommandNode.java
+++
@@ -1,9 +0,0 @@
-package lumCode.folderScriptInterpreter.handlers.command;
-
-import lumCode.folderScriptInterpreter.handlers.Node;
-import lumCode.folderScriptInterpreter.variables.Variable;
-
-public interface CommandNode extends Node {
-
-	public Variable[] result();
-}
--- src/lumCode/folderScriptInterpreter/handlers/logic/LogicNode.java
+++
@@ -1,8 +0,0 @@
-package lumCode.folderScriptInterpreter.handlers.logic;
-
-import lumCode.folderScriptInterpreter.handlers.Node;
-
-public interface LogicNode extends Node {
-
-	public boolean result();
-}
