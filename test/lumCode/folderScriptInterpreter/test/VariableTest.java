01c14a22d7dc3bc4b801d1c03efabe5f0238c3d4
MrLumme <mclumme@gmail.com>
2021-06-04 15:47:43 +0200

    - Added VariableTest

--- test/lumCode/folderScriptInterpreter/test/VariableTest.java
+++ test/lumCode/folderScriptInterpreter/test/VariableTest.java
@@ -6,17 +6,17 @@ import org.junit.jupiter.api.Test;
 
 import lumCode.folderScriptInterpreter.variables.FileVariable;
 import lumCode.folderScriptInterpreter.variables.FolderVariable;
-import lumCode.folderScriptInterpreter.variables.IntVariable;
+import lumCode.folderScriptInterpreter.variables.NumberVariable;
 import lumCode.folderScriptInterpreter.variables.SpecialVariable;
-import lumCode.folderScriptInterpreter.variables.StringVariable;
+import lumCode.folderScriptInterpreter.variables.TextVariable;
 import lumCode.folderScriptInterpreter.variables.Variable;
 
 class VariableTest {
 
 	@Test
 	void interpretationTest() {
-		assertTrue(Variable.fromString("-390586") instanceof IntVariable);
-		assertTrue(Variable.fromString("fpotpk") instanceof StringVariable);
+		assertTrue(Variable.fromString("-390586") instanceof NumberVariable);
+		assertTrue(Variable.fromString("fpotpk") instanceof TextVariable);
 		assertTrue(Variable.fromString("c:/users/fsi/test.pdf") instanceof FileVariable);
 		assertTrue(Variable.fromString("c:/users/fsi/") instanceof FolderVariable);
 		assertTrue(Variable.fromString("$") instanceof SpecialVariable);
