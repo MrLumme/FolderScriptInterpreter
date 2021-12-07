1d3182f1b05ca38bfcae78ee1b285bb4ce1ab621
MrLumme <mclumme@gmail.com>
2021-12-07 15:29:52 +0100

    - Updated InterpretationTest

--- test/lumCode/folderScriptInterpreter/test/InterpretationTest.java
+++ test/lumCode/folderScriptInterpreter/test/InterpretationTest.java
@@ -20,19 +20,41 @@ public class InterpretationTest {
 		assertTrue(new File(tf1.getAbsolutePath() + "/dok3.xml").exists());
 	}
 
+	private void clear() {
+		Main.a.clear();
+		Main.i.clear();
+		Main.v.clear();
+		Main.nodes.clear();
+	}
+
 	@Test
 	void test1() throws InterpreterException {
-		// Test0 - Declare and print variable to screen
+		// Test1 - Declare and print variable to screen
+		System.out.println("Test1");
+		clear();
 		Main.main(new String[] { "#var=45,o(#var,$)" });
+		System.out.println();
 	}
 
 	@Test
 	void test2() throws InterpreterException {
-		// Test1 - List filenames into a new text file
+		// Test2 - List filenames into a new text file
+		System.out.println("Test2");
 		File list = new File(tf1.getAbsolutePath() + "/list.txt");
+		clear();
 		Main.main(
 				new String[] { "i0(a[0]){o(n(i0)+e(i0)+\"\n\",a[1])}", tf1.getAbsolutePath(), list.getAbsolutePath() });
 		assertTrue(list.exists());
+		System.out.println();
+	}
+
+	@Test
+	void test3() throws InterpreterException {
+		// Test3 - List an array of different variables
+		System.out.println("Test3");
+		clear();
+		Main.main(new String[] { "#var[0]=45,#var[1]=\"eeeeE\",#var[2]!1,i0(#var){o(i0+\"\n\",$)}" });
+		System.out.println();
 	}
 
 }
