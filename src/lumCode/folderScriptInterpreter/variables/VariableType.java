89a508925c14f175e94d24907a4bdeb84da0b1eb
MrLumme <mclumme@gmail.com>
2022-02-25 10:36:07 +0100

    - Added toString method to VariableType

--- src/lumCode/folderScriptInterpreter/variables/VariableType.java
+++ src/lumCode/folderScriptInterpreter/variables/VariableType.java
@@ -1,5 +1,10 @@
 package lumCode.folderScriptInterpreter.variables;
 
 public enum VariableType {
-	FILE, FOLDER, NUMBER, TEXT, SPECIAL, ARRAY
+	FILE, FOLDER, NUMBER, TEXT, SPECIAL, ARRAY;
+
+	@Override
+	public String toString() {
+		return name().toLowerCase();
+	}
 }
