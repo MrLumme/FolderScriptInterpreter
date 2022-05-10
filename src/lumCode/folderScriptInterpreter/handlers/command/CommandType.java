87330a7110e696d45d7aba745270e368c176e1d3
MrLumme <mclumme@gmail.com>
2022-05-10 11:30:21 +0200

    - Made isBooleanOutput and isOutput method in CommandType work on EnumSet instead of fields

--- src/lumCode/folderScriptInterpreter/handlers/command/CommandType.java
+++ src/lumCode/folderScriptInterpreter/handlers/command/CommandType.java
@@ -1,31 +1,24 @@
 package lumCode.folderScriptInterpreter.handlers.command;
 
+import java.util.EnumSet;
+
 import lumCode.folderScriptInterpreter.exceptions.typeExceptions.UnsupportedTypeException;
 
 public enum CommandType {
-	NAME('n', 1), EXTENSION('e', 1), PARENT('p', 1), IS_FILE('f', 1, true, true), IS_AVAILABLE('v', 1, true, true),
-	COPY('c', 2, true, true), MOVE('m', 2, true, true), DELETE('d', 1, true, true), LIST('l', 2), WRITE('w', 2, false),
-	SIZE('s', 1), REPLACE('y', 3), SUBSTRING('u', 3), RANDOM('q', 1), READ('r', 1), OPTIONS('o', 2, false),
-	SLEEP('z', 1, false), EXIT('x', 1, false), GEN_MD5('g', 1);
+	NAME('n', 1), EXTENSION('e', 1), PARENT('p', 1), IS_FILE('f', 1), IS_AVAILABLE('v', 1), COPY('c', 2), MOVE('m', 2),
+	DELETE('d', 1), LIST('l', 2), WRITE('w', 2), SIZE('s', 1), REPLACE('y', 3), SUBSTRING('u', 3), RANDOM('q', 1),
+	READ('r', 1), OPTIONS('o', 2), SLEEP('z', 1), EXIT('x', 1), GEN_MD5('g', 1);
+
+	public static EnumSet<CommandType> booleanOutputs = EnumSet.of(IS_AVAILABLE, IS_FILE, COPY, MOVE, DELETE);
+	public static EnumSet<CommandType> outputs = EnumSet.of(NAME, EXTENSION, PARENT, LIST, SIZE, REPLACE, SUBSTRING,
+			RANDOM, READ, GEN_MD5);
 
 	private char symbol;
 	private int input;
-	private boolean output;
-	private boolean boolOut;
 
 	private CommandType(char symbol, int input) {
-		this(symbol, input, true, false);
-	}
-
-	private CommandType(char symbol, int input, boolean output) {
-		this(symbol, input, output, false);
-	}
-
-	private CommandType(char symbol, int input, boolean output, boolean boolOut) {
 		this.symbol = symbol;
 		this.input = input;
-		this.output = output;
-		this.boolOut = boolOut;
 	}
 
 	public static CommandType fromChar(char c) throws UnsupportedTypeException {
@@ -55,10 +48,11 @@ public enum CommandType {
 	}
 
 	public boolean isOutput() {
-		return output;
+		return outputs.contains(this) || isBooleanOutput();
 	}
 
 	public boolean isBooleanOutput() {
-		return boolOut;
+		return booleanOutputs.contains(this);
+	}
 	}
 }
