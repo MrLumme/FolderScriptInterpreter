f0e28abac753a6c07847aa85df2834c06dd2889a
Morten Thomsen <morten.l.thomsen@cgi.com>
2025-11-05 14:40:11 +0100

    Removed unused exception throw

--- src/lumCode/folderScriptInterpreter/exceptions/typeExceptions/IncorrectParameterTypeException.java
+++ src/lumCode/folderScriptInterpreter/exceptions/typeExceptions/IncorrectParameterTypeException.java
@@ -7,7 +7,7 @@ import lumCode.folderScriptInterpreter.variables.Variable;
 public class IncorrectParameterTypeException extends InterpreterException {
 	private static final long serialVersionUID = 4295363097012846149L;
 
-	public IncorrectParameterTypeException(CommandType c, Variable v) throws UnsupportedCommandTypeException {
+	public IncorrectParameterTypeException(CommandType c, Variable v) {
 		super("The variable '" + v.toString() + "' resolves to the type '" + v.type.toString().toLowerCase()
 				+ "' which is not supported for the command '" + c.getChar() + "'.");
 	}
