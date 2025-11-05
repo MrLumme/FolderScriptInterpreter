32632a9549b821b20142addd23a6b1fcbcb9afc0
Morten Thomsen <morten.l.thomsen@cgi.com>
2025-11-05 13:51:48 +0100

    Fixed spelling in NotFoundException

--- src/lumCode/folderScriptInterpreter/exceptions/notFoundExceptions/NotFoundException.java
+++ src/lumCode/folderScriptInterpreter/exceptions/notFoundExceptions/NotFoundException.java
@@ -6,11 +6,11 @@ public class NotFoundException extends InterpreterException {
 	private static final long serialVersionUID = 965004424139859568L;
 
 	public NotFoundException(String type, String name) {
-		super("Could not found a " + type + " named '" + name + "'.");
+		super("Could not find a " + type + " named '" + name + "'.");
 	}
 
 	public NotFoundException(String type, int number) {
-		super("Could not found an " + type + " with the number '" + number + "'.");
+		super("Could not find an " + type + " with the number '" + number + "'.");
 	}
 
 }
